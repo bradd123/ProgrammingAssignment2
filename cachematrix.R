fb4a156a4ed89440d22cbf2ea00d8de75fe99108
brahma <brahma@gopassport.com>
2016-01-25 00:15:54 +0530

    Write two functions to inverse the matrix and get cached data

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix return a list in which each element is a function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      ## initially NULL, holds cached value
+      cache <- NULL
+      
+      ## stores the matrix
+      setMatrix <- function(y) {
+            x <<- y
+            
+            cache <<- NULL
+      }
+      
+      ## get stored matrix
+      getMatrix <- function() {
+            x
+      }
+      
+      ## cache the argument
+      setInverse <- function(s) {
+            cache <<- s
+      }
+      
+      ## get cached value
+      getInverse <- function() {
+            cache
+      }
+      
+      ## return all functions
+      list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Calculates the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        # get cached value
+        inverse <- x$getInverse()
+        # check whether cached value exists or not
+        # if exists return the value
+        if(!is.null(inverse)){
+            message("getting cached data")
+            return(inverse)
+        }
+        # else get matrix, calculate inverse and store cache
+        data <- x$getMatrix()
+        inverse <- solve(data)
+        x$setInverse(inverse)
+        # return the inverse value
+        inverse
 }
